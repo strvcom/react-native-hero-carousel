f93b1cc4b9b8672d79aa594e05caf275d79ee862
Petr Konecny <petr.konecny@strv.com>
2025-07-28 11:52:57 +0200

    chore: rename

--- example/examples/components/CarouselBase.tsx
+++ example/examples/components/CarouselBase.tsx
@@ -1,19 +1,19 @@
 import {
-  AutoCarousel,
-  AutoCarouselProps,
+  HeroCarousel,
+  HeroCarouselProps,
   CarouselContextProvider,
 } from '@strv/react-native-hero-carousel'
 import { SafeAreaView, StyleSheet, View } from 'react-native'
 import { Stack } from 'expo-router'
 import { Pagination } from '@/examples/components/Pagination'
 
-export function CarouselBase({ children }: { children: AutoCarouselProps['children'] }) {
+export function CarouselBase({ children }: { children: HeroCarouselProps['children'] }) {
   return (
     <CarouselContextProvider>
       <Stack.Screen options={{ title: 'Animated Carousel' }} />
       <SafeAreaView style={styles.container}>
         <View style={styles.container}>
-          <AutoCarousel>{children}</AutoCarousel>
+          <HeroCarousel>{children}</HeroCarousel>
           <Pagination total={children.length} />
         </View>
       </SafeAreaView>
